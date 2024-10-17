7cb985f49a7f1f1e3ef21fc4947b2c3b29852ab2
pbumihome <pbumidan@DESKTOP-6RNAT92>
2024-10-17 17:33:49 +0300

    parser 75%

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: pbumidan <pbumidan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 20:36:10 by pbumidan          #+#    #+#             */
-/*   Updated: 2024/10/15 20:39:00 by pbumidan         ###   ########.fr       */
+/*   Updated: 2024/10/17 17:28:14 by pbumidan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,8 +47,7 @@ int	check_file(int argc, char **argv)
 			ft_putstr_fd("Error\n* Invalid file *\n\n", 2);
 			return (0);
 		}
-		else if (valid_cub(argv[1]))
-			return (1);
+		return (1);
 	}
 	return (0);
 }
@@ -71,15 +70,71 @@ char *get_substr(char *line, int start)
 	return (substr);
 }
 
+size_t  arr_size(char **arr)
+{
+    size_t count;
+    
+    count = 0;
+    while (arr[count] != NULL)
+    {
+        count++;
+    }
+    return (count);
+}
+
+bool check_colors(char **colors)
+{
+    if (ft_atoi(colors[0]) < 0 || ft_atoi(colors[0]) > 255)
+        return false;
+    if (ft_atoi(colors[1]) < 0 || ft_atoi(colors[1]) > 255)
+        return false;
+    if (ft_atoi(colors[2]) < 0 || ft_atoi(colors[2]) > 255)
+        return false;
+    return true;
+}
+
+bool get_rgb(char *line, t_fc *fc)
+{
+    char **colors;
+    ft_putstr("LINEEEEEEEEEEE ");
+    ft_putstr(line);
+    ft_putstr("\n");
+	colors = ft_split(line, ',');
+    if (!colors)
+    {
+        ft_putstr_fd("Error\n* Memory allocation failed for colors *\n\n", 2);
+        return false;
+    }
+    if ((arr_size(colors) != 3) || (check_colors(colors) == false))
+    {
+        ft_putstr_fd("Error\n* invalid parmeters for colors *\n\n", 2);
+        int i = 0; // free
+        while (colors[i])
+            free(colors[i++]);
+        free(colors);
+        return false;
+    }
+    fc->R = ft_atoi(colors[0]);
+    fc->G = ft_atoi(colors[1]);
+    fc->B = ft_atoi(colors[2]);
+    int i = 0; // free
+    while (colors[i])
+        free(colors[i++]);
+    free(colors);
+	return (true);
+}
+
 bool is_space(char c)
 {
-	return (c == ' ' || (c >= 9 && c <= 13));
+    if (c == ' ' || (c >= 9 && c <= 13))
+        return true;
+    return false;
 }
 
 bool check_fc(int fd, t_main *game)
 {
-	char *line = NULL;
-	
+	char *line;
+	ft_putstr("went to FCCCC \n");
 	game->ceil = malloc(sizeof(t_fc));
 	if (!game->ceil)
 	    {
@@ -87,28 +142,69 @@ bool check_fc(int fd, t_main *game)
         close(fd);
         return false;
     }
-	  while ((line = get_next_line(fd)) != NULL)
+    game->floor = malloc(sizeof(t_fc));
+    if (!game->floor)
+	{
+        ft_putstr_fd("Error\n* Memory allocation failed for floor *\n\n", 2);
+        close(fd);
+        return false;
+    }
+	while ((line = get_next_line(fd)) != NULL)
     {
-        ft_putstr("test: ");
+        ft_putstr("line :");
         ft_putstr(line);
         ft_putstr("\n");
-        if (ft_strncmp(line, "F", 1) == 0 && !game->walls->NO)
+        if (ft_strncmp(line, "C", 1) == 0)
 		{
-            if (is_space(line[2]))
+            if (is_space(line[1]) == true)
             {
-                ft_putstr("b4sub\n");
-                game->walls->NO = get_substr(line, 3); // Start after "NO "
+                 ft_putstr("VVVVVVV");
+                if (get_rgb(get_substr(line, 2), game->ceil) == false)
+                {
+                    free(line);
+                    close(fd);
+                    return false;
+                }
+                ft_putstr("CEIL: ");
+                ft_putstr(line);
+                ft_putstr("\n");
                 free(line);
             }
             else
             {
-                ft_putstr_fd("Error\n* Invalid format for NO wall *\n\n", 2);
+                ft_putstr_fd("Error\n* Invalid format for ceiling wall *\n\n", 2);
                 free(line);
                 close(fd);
                 return false;
             }
         }
-
+        if (ft_strncmp(line, "F", 1) == 0)
+		{
+            if (is_space(line[1]) == true)
+            {
+                
+                if (get_rgb(get_substr(line, 2), game->floor) == false)
+                {
+                    free(line);
+                    close(fd);
+                    return false;
+                }
+                    ft_putstr("FLOOR: ");
+                    ft_putstr(line);
+                    ft_putstr("\n");
+                free(line);
+            }
+            else
+            {
+                ft_putstr_fd("Error\n* Invalid format for ceiling wall *\n\n", 2);
+                free(line);
+                close(fd);
+                return false;
+            }
+        }
+	}
+    close(fd);
+    return true;
 }
 
 bool check_walls(int fd, t_main *game)
@@ -140,7 +236,7 @@ bool check_walls(int fd, t_main *game)
         ft_putstr("\n");
         if (ft_strncmp(line, "NO", 2) == 0 && !game->walls->NO)
 		{
-            if (is_space(line[2]))
+            if (is_space(line[2]) == true)
             {
                 ft_putstr("b4sub\n");
                 game->walls->NO = get_substr(line, 3); // Start after "NO "
@@ -156,7 +252,7 @@ bool check_walls(int fd, t_main *game)
         }
         else if (ft_strncmp(line, "SO", 2) == 0 && !game->walls->SO)
         {
-            if (is_space(line[2]))
+            if (is_space(line[2]) == true)
             {
                 ft_putstr("b4sub\n");
                 game->walls->SO = get_substr(line, 3); // Start after "NO "
@@ -172,7 +268,7 @@ bool check_walls(int fd, t_main *game)
         }
         else if (ft_strncmp(line, "WE",2) == 0 && !game->walls->WE)
         {
-            if (is_space(line[2]))
+            if (is_space(line[2]) == true)
             {
                 ft_putstr("b4sub\n");
                 game->walls->WE = get_substr(line, 3); // Start after "NO "
@@ -188,7 +284,7 @@ bool check_walls(int fd, t_main *game)
         }
         else if (ft_strncmp(line, "EA", 2) == 0 && !game->walls->EA)
         {
-            if (is_space(line[2]))
+            if (is_space(line[2]) == true)
             {
                 ft_putstr("b4sub\n");
                 game->walls->EA = get_substr(line, 3); // Start after "NO "
@@ -238,12 +334,21 @@ void initialize_game(char *cubfile, t_main *game)
         exit(1);
     }
 	if (check_walls(fd, game) == false)
-		exit(1);//
+		exit(1);
+    close (fd);
+    fd = open(cubfile, O_RDONLY);
+    if (fd < 0)
+    {
+        ft_putstr_fd("Error\n* Error opening file *\n\n", 2);
+        exit(1);
+    }
 	if (check_fc(fd, game) == false)
 		exit (1);
+    close(fd);
 	
 }
 
+#include <stdio.h>
 int main(int argc, char **argv)
 {
 	t_main game;
@@ -252,15 +357,17 @@ int main(int argc, char **argv)
 		exit (1);
 	initialize_game(argv[1], &game);
 	ft_putstr("WOW\n");
-	ft_putstr(game.walls->NO);
-	ft_putstr("\n");
-	ft_putstr(game.walls->SO);
-	ft_putstr("\n");
-	ft_putstr(game.walls->WE);
-	ft_putstr("\n");
-	ft_putstr(game.walls->EA);
-	ft_putstr("\n");
+	// ft_putstr(game.walls->NO);
+	// ft_putstr("\n");
+	// ft_putstr(game.walls->SO);
+	// ft_putstr("\n");
+	// ft_putstr(game.walls->WE);
+	// ft_putstr("\n");
+	// ft_putstr(game.walls->EA);
+	// ft_putstr("\n");
 	// Free allocated memory
+    printf("F: %d, %d, %d \n", game.floor->R, game.floor->G, game.floor->B);
+    printf("C: %d, %d, %d \n", game.ceil->R, game.ceil->G, game.ceil->B);
     free(game.walls->NO);
     free(game.walls->SO);
     free(game.walls->WE);
